6145589ca76b3efee2e5f6ae0bba737e405b364d
Albert Font <afont@disgrafic.com>
2019-09-19 10:52:57 +0200

    Update the application to new version 1.0.0

---
+++ Installation/SQLVersions/01.00.00.SqlDataProvider.sql
@@ -0,0 +1,8 @@
+﻿
+/* ----------------------------------------------------------------------------
+  New field NewColumn to Arv_ChangeRequest
+------------------------------------------------------------------------------- */
+-- 
+IF NOT EXISTS(SELECT c.* FROM syscolumns c JOIN sysobjects o ON o.id=c.id WHERE c.[name]='NewColumn' AND o.name ='Arv_ChangeRequest')
+	ALTER TABLE Arv_ChangeRequest ADD NewColumn nvarchar(150)
+GO
